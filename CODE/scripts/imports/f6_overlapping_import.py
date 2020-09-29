1126ecb39a74568a846d854c15f3e7a7f72ce972
leangaurav <trainer.cpp@gmail.com>
2020-09-29 10:06:35 +0530

    Session 04 scripts

---
+++ CODE/scripts/imports/f6_overlapping_import.py
@@ -0,0 +1,5 @@
+import cmath
+print(cmath.sqrt(-1))
+
+import math
+print(math.sqrt(-1))
---
+++ CODE/scripts/imports/f8_nested_modules.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+datetime.now()
+#from datetime.datetime import now
+#now()
+#
+#import datetime.datetime as dt
+#dt.now()
+import datetime
+print(dir(datetime))
+datetime.datetime.now()
+
