695881b5bb58d9e5430ebff9e1cd957366c140e2
Meenakshi Vashistha <15meenakshi96@gmail.com>
2020-10-01 10:08:06 +0530

    delete

--- CODE/scripts/f4_module_alias.py
+++
@@ -1,7 +0,0 @@
-import random as rr  # alias name for random module as rr
-
-print(rr.randint(10,20)) # 10-20  included
-
-print(rr.randrange(10)) # 0 - 9 # start - (end -1)
-print(rr.randrange(10,20)) # 10 - 19 # start - (end -1)
-print(rr.randrange(10,20,3)) # 10, 13, 16, 19 
